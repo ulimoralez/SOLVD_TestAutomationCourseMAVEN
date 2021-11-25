881885659db82201b52d8e0b658f6e8a73675fd7
Ulises Moralez <moralezulises@gmail.com>
2021-11-25 04:57:11 -0300

    Added task2 - Uses FileUtils and StringUtils methods

--- src/main/java/com/solvd/readtext/TextManipulator.java
+++ src/main/java/com/solvd/readtext/TextManipulator.java
@@ -12,20 +12,40 @@ import java.util.List;
 
 public class TextManipulator {
     public static void main(String[] args) {
-        File file = new File("src/main/java/com/solvd/readtext/JavaWikipedia.txt");
+        File file = new File("src/main/java/com/solvd/readtext/AlanTuringWikipedia.txt");
         String wordCounter;
         String fileContent = readLinesFromFile(file.getPath());
 
-        System.out.println(fileContent);
+        System.out.println("Original content"+fileContent);
 
         wordCounter = String.valueOf(StringUtils.split(fileContent, " ").length);
 
+        //try {
+        //    FileUtils.writeStringToFile(file, fileContent + wordCounter, StandardCharsets.UTF_8);
+        //}catch (IOException e){
+        //    e.printStackTrace();
+        //}
+        fileContent = readLinesFromFile(file.getPath());
+        //Task 2
+        //StringUtils methods
+        System.out.println("\nReverse content: "+StringUtils.reverse(fileContent));
+        System.out.println("\nRemove the letter a: "+StringUtils.remove(fileContent, "a"));
+        System.out.println("\nCount every time a appears: "+StringUtils.countMatches(fileContent, "a"));
+        System.out.println("\nDelete whitespaces: "+StringUtils.deleteWhitespace(fileContent));
+        System.out.println("\nUppercase content: "+ StringUtils.upperCase(fileContent));
+
+        //FileUtils Methods
+        File copyText = new File("src/main/java/com/solvd/readtext/copytext.txt");
+        File newPackage = new File("src/main/java/com/solvd/readtext/newpackage");
         try {
-            FileUtils.writeStringToFile(file, fileContent + wordCounter, StandardCharsets.UTF_8);
-        }catch (IOException e){
+            FileUtils.copyFile(file, copyText);
+            FileUtils.forceMkdir(newPackage);
+            FileUtils.lastModified(copyText);
+            System.out.println(FileUtils.getUserDirectoryPath());
+            System.out.println(FileUtils.sizeOf(file));
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        fileContent = readLinesFromFile(file.getPath());
     }
     public static String readLinesFromFile(String path){
         File file = new File(path);
