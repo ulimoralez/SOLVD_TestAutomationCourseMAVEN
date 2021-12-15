df77ec8ee48d4159be8aa2da6c8cec0a33cf3526
Ulises Moralez <moralezulises@gmail.com>
2021-12-15 16:34:03 -0300

    Homework 10 - Added Connection - connectionPool - ClientsRunner. Run ClientsRunner

---
+++ src/main/java/com/solvd/homework/connection/Connection.java
@@ -0,0 +1,28 @@
+package com.solvd.homework.connection;
+
+import java.util.logging.Logger;
+
+public class Connection {
+    private final Logger LOGGER = Logger.getLogger(Connection.class.getName());
+
+    public void read(String threadName){
+        LOGGER.info("The read query: in thread "+threadName+" was executed successfully!");
+    }
+    public void delete(String threadName){
+        LOGGER.info("The delete query: in thread"+threadName+" was executed successfully!");
+    }
+    public void update(String threadName){
+        LOGGER.info("The update query: in thread"+threadName+" was executed successfully!");
+    }
+    public void create(String threadName){
+        LOGGER.info("The create query: in thread"+threadName+" was executed successfully!");
+    }
+
+    public void executeCRUD(String threadName){
+        this.create(threadName);
+        this.read(threadName);
+        this.update(threadName);
+        this.delete(threadName);
+    }
+
+}
